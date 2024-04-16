866cdddf7107141ad160a5e93104909a7a765f03
dmkomur <125804681+dmkomur@users.noreply.github.com>
2024-04-16 20:29:35 +0200

    basic layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+recipes: Recipe[] = [new Recipe('Borsch', 'Good yummi sup with meet and vegs', 'https://images.immediate.co.uk/production/volatile/sites/30/2022/10/Ukrainian-pork-rib-borsch--83702a8.jpg?quality=90&resize=556,505'), new Recipe('Schy', 'Good yummi sup with meet and vegs', 'https://images.immediate.co.uk/production/volatile/sites/30/2022/10/Ukrainian-pork-rib-borsch--83702a8.jpg?quality=90&resize=556,505')]
 }
