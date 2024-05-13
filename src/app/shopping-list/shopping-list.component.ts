c2557a574237d52fbd3d64f219f529a9cc42c4f0
dmkomur <125804681+dmkomur@users.noreply.github.com>
2024-05-13 20:45:19 +0200

    used subject

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { Ingridient } from './ingridient.model';
 import { ShopppingService } from './shopping.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent implements OnInit{
+export class ShoppingListComponent implements OnInit, OnDestroy{
   ingridients: Ingridient[]
+  private ingSubscription: Subscription
   constructor(private shoppingService: ShopppingService) { }
   ngOnInit(): void {
     this.ingridients = this.shoppingService.getIngridients()
-    this.shoppingService.updatedIngridients.subscribe((data: Ingridient[]) => this.ingridients = data
+    this.ingSubscription = this.shoppingService.updatedIngridients.subscribe((data: Ingridient[]) => this.ingridients = data
     )
   }
-
+ngOnDestroy(): void {
+  this.ingSubscription.unsubscribe()
+}
 }
--- src/app/shopping-list/shopping.service.ts
+++ src/app/shopping-list/shopping.service.ts
@@ -1,17 +1,17 @@
-import { EventEmitter } from '@angular/core';
 import { Ingridient } from "./ingridient.model"
+import { Subject } from 'rxjs';
 export class ShopppingService {
         ingridients: Ingridient[] = [{ name: "apple", amount: 6 }, { name: "gul", amount: 16 }]
-        updatedIngridients = new EventEmitter<Ingridient[]>()
+        updatedIngridients = new Subject<Ingridient[]>()
         getIngridients() {
                 return this.ingridients.slice()
         }
         addIngridient(el: Ingridient) {
                 this.ingridients.push(el)
-                this.updatedIngridients.emit(this.ingridients.slice())
+                this.updatedIngridients.next(this.ingridients.slice())
         }
         addIngridients(data: Ingridient[]) {
                 this.ingridients.push(...data)
-                this.updatedIngridients.emit(this.ingridients.slice())
+                this.updatedIngridients.next(this.ingridients.slice())
         }
 }
